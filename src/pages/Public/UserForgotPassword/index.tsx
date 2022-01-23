82bb28a0a02a1fee64eeb78d7e87e1c8dfedca1e
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2022-01-23 23:15:31 +0800

    Added som fixes and comments

--- src/pages/Public/UserForgotPassword/index.tsx
+++ src/pages/Public/UserForgotPassword/index.tsx
@@ -1,39 +1,46 @@
-import { Formik ,Form,Field} from 'formik';
-import axios from 'axios';
-import {Link} from 'react-router-dom'
-import useAuth from 'hooks/useAuth';
-import request from 'api/request';
+// 1. external libraries
+import { Formik, Form, Field, FormikHelpers } from 'formik'
+import * as yup from 'yup'
 
+// 2. utility files
+import request from 'api/request'
 
+// 3. components
 
-interface MyFormValues {
-    email: string;
-    
+// 4. css
+// import styles from ...
+
+interface Values {
+  email: string
 }
-
 
 const UserForgotPassword = () => {
-    const loginURL = "";
-    const initialValues: MyFormValues = {email:'' };
+  const loginURL = ''
+  const initialValues: Values = { email: '' }
 
-    return (
+  const validationSchema: yup.SchemaOf<Values> = yup.object({
+    email: yup.string().required('Required'),
+  })
+
+  return (
     <div>
-        <h1 >Forgot Password</h1> 
-        <Formik
-         initialValues={initialValues}
-         onSubmit={(values, actions) => {
-           console.log({ values, actions });
-           request.post(loginURL,{"email":values.email});
-         }}
-         >
-         <Form>
-           <label htmlFor="email">UserName</label>
-           <Field id="email" name="UserName" placeholder="email" />
-           <button type="submit">Submit</button>
-         </Form>
-       </Formik>       
+      <h1>Forgot Password</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values: Values, formikHelpers: FormikHelpers<Values>) => {
+          console.log({ values, formikHelpers })
+          request.post(loginURL, { email: values.email })
+        }}
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <label htmlFor="email">UserName</label>
+          <Field id="email" name="email" placeholder="email" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </div>
-    )
+  )
 }
 
-export default UserForgotPassword;
+export default UserForgotPassword
